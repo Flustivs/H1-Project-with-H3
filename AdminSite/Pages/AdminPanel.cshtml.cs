b0b7dc627ca390b593fa04a0e700280badb5a17d
Flustivs <rune170206@gmail.com>
2023-10-09 12:47:03 +0200

    Rune

--- AdminSite/Pages/AdminPanel.cshtml.cs
+++ AdminSite/Pages/AdminPanel.cshtml.cs
@@ -31,7 +31,7 @@ namespace AdminSite.Pages
             _personmanager = personManager;
             _logInManager = logInManager;
             _facilityManager = facilityManager;
-            _priceManager = priceManager;
+            ticketManager = priceManager;
         }
 
         public void OnGet()
@@ -62,21 +62,64 @@ namespace AdminSite.Pages
             AllFacilities = _facilityManager.GetAllFacilities();
         }
 
-            public string Price1()
-            {
-                string price = _priceManager.TicketsConnect(1);
-                return price;
-            }
-            public string Price2()
-            {
-                string price = _priceManager.TicketsConnect(2);
-                return price;
-            }
-            public string Price3()
-            {
-                string price = _priceManager.TicketsConnect(3);
-                return price;
-
-            }
+        //public IActionResult OnPostEdit()
+        //{
+        //    // Use _personmanager to retrieve the details of the selected staff member
+        //    // And render an edit form
+        //}
+        //public IActionResult OnPostDelete()
+        //{
+        //    // Use _personmanager to delete the selected staff member
+        //    _personmanager.DeletePerson(SelectedStaffEmail);
+        //}
+        //public IActionResult OnPostAddNew()
+        //{
+        //    // Render a form to add a new staff member
+        //}
+    }
+}
+        {
+            // Use _personmanager to retrieve the details of the selected staff member
+            // And render an edit form
+        }
+        public IActionResult OnPostDelete()
+        {
+            // Use _personmanager to delete the selected staff member
+            _personmanager.DeletePerson(SelectedStaffEmail);
+        }
+        public IActionResult OnPostAddNew()
+        {
+            // Render a form to add a new staff member
         }
     }
+}
+        {
+            // Use _personmanager to retrieve the details of the selected staff member
+            // And render an edit form
+        }
+        public IActionResult OnPostDelete()
+        {
+            // Use _personmanager to delete the selected staff member
+            _personmanager.DeletePerson(SelectedStaffEmail);
+        }
+        public IActionResult OnPostAddNew()
+        {
+            // Render a form to add a new staff member
+        }
+    }
+}
+        {
+            // Use _personmanager to retrieve the details of the selected staff member
+            // And render an edit form
+        }
+        public IActionResult OnPostDelete()
+        {
+            // Use _personmanager to delete the selected staff member
+            _personmanager.DeletePerson(SelectedStaffEmail);
+        }
+        public IActionResult OnPostAddNew()
+        {
+            // Render a form to add a new staff member
+        }
+    }
+}
