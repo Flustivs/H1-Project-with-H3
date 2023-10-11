42b948c77f7b0bb66ef538f3191c39b79f8c0e43
Tian707 <122786950+Tian707@users.noreply.github.com>
2023-10-11 08:53:28 +0200

    Update FacilityAddNew.cshtml.cs

--- AdminSite/Pages/FacilityAddNew.cshtml.cs
+++ AdminSite/Pages/FacilityAddNew.cshtml.cs
@@ -5,8 +5,35 @@ namespace AdminSite.Pages
 {
     public class FacilityAddNewModel : PageModel
     {
+    private readonly FacilityManager _facilityManager;
+public FacilityAddNewModel(FacilityManager facilityManager)
+{
+    _facilityManager = facilityManager;
+}
+
+[BindProperty]
+public string NewName { get; set; }
+[BindProperty]
+public string NewURL { get; set; }
+
+[BindProperty]
+public DateTime NewDate { get; set; }
+
         public void OnGet()
         {
         }
+
+         public IActionResult OnPostSaveChanges()
+ {
+     _facilityManager.AddFacility(NewName, NewURL, NewDate);
+
+     return RedirectToPage("/AdminPanel");
+ }
+
+ public IActionResult OnPostCancel()
+ {
+
+     return RedirectToPage("/AdminPanel");
+ }
     }
 }
